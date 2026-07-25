fb789348757650216ffa4ffa8ac3c4381cb4d02d
Zaoth OS <fylluszophia@proton.me>
2026-07-25 05:01:33 -0300

    fix: update correct cp reference

--- files/scripts/install_bashlib.sh
+++ files/scripts/install_bashlib.sh
@@ -5,6 +5,4 @@ apt install git -y &>/dev/null
 
 git clone https://github.com/fyllus/bashlib
 
-mkdir -p files/system/usr/lib
-
-cp -r bashlib/bash files/system/usr/lib/
+cp -r bashlib/bash /usr/lib/
